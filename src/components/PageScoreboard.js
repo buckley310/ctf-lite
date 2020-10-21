6212c4b2601eebd5f62d0df83192541c395824df
Sean Buckley <sean.bck@gmail.com>
2020-10-21 00:53:36 -0400

    display scoreboard

--- src/components/PageScoreboard.js
+++ src/components/PageScoreboard.js
@@ -1,51 +1,36 @@
 import React from 'react';
+import { Api, loadingError } from '../lib/api.js';
+import styles from './PageScoreboard.module.css';
 
-function PageScoreboard() {
-    return (
-        <div className="capWidth">
-            <h1>scoreboard - top 10</h1>
-            <table id="scoreboard">
-                <thead>
-                    <tr>
-                        <th width='1'>placement</th>
-                        <th>username</th>
-                        <th>points</th>
-                    </tr>
-                </thead>
-                <tbody id="scoreTable">
-                </tbody>
-            </table>
-            {/* <script>
-                function contentInit() {
-                    api('scoreboard').then(function (j) {
-                        let stripes = true;
-                        for (let i in j) {
-                            let tr = document.createElement('tr');
-                            tr.addEventListener('click', () => location.assign('/profile/#' + j[i]['_id']));
-
-                            let place = document.createElement('td');
-                            let name = document.createElement('td');
-                            let points = document.createElement('td');
-
-                            place.textContent = i;
-                            name.textContent = j[i]['username'];
-                            points.textContent = j[i]['score'];
-
-                            tr.appendChild(place);
-                            tr.appendChild(name);
-                            tr.appendChild(points);
-
-                            if (stripes)
-                                tr.style.backgroundColor = "rgba(0,0,0,.05)"
-                            stripes = !stripes;
-
-                            document.getElementById('scoreTable').appendChild(tr);
-                        }
-                    }).catch(loadingError);
-                }
-            </script> */}
-        </div>
-    );
+export default class PageScoreboard extends React.Component {
+    state = { scoreboard: [] };
+    viewProfile = id => {
+        alert(`TODO: ${id}`);
+    };
+    render() {
+        return (
+            <div className="capWidth">
+                <h1>scoreboard - top 10</h1>
+                <table className={styles.scoreboard}>
+                    <thead>
+                        <tr>
+                            <th width='1'></th>
+                            <th>username</th>
+                            <th>points</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.scoreboard.map(entry => (
+                            <tr key={entry.username} onClick={() => this.viewProfile(entry._id)}>
+                                <td></td>
+                                <td>{entry.username}</td>
+                                <td>{entry.score}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+    componentDidMount = () => Api('scoreboard').then(x => this.setState({ scoreboard: x })).catch(loadingError);
 }
-
-export default PageScoreboard;
